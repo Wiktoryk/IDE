f3edf10f823be8927a40562a692eb102de0946bf
Wiktoryk <242486@edu.p.lodz.pl>
2025-11-10 23:21:03 +0100

    text buffer interface

---
+++ src/buffer/textBuffer.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <QString>
+#include <QStringView>
+#include <vector>
+
+class ITextBuffer {
+public:
+	virtual ~ITextBuffer() = default;
+
+	virtual void clear() = 0;
+	virtual qsizetype size() const = 0;
+	virtual void insert(qsizetype pos, QStringView stringview) = 0;
+	virtual void erase(qsizetype pos, qsizetype len) = 0;
+
+	virtual QString slice(qsizetype pos, qsizetype len) const = 0;
+	virtual QString toString() const = 0;
+	virtual qsizetype lineCount() const = 0;
+	virtual qsizetype lineStart(qsizetype line) const = 0;
+	virtual qsizetype positionFromLineCol(qsizetype line, qsizetype col) const = 0;
+};
